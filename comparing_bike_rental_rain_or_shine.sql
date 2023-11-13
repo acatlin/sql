cb285f96a0f560957495be9a9f8a4f31a4c16f50
acatlin <Andrew.Catlin@sps.cuny.edu>
2023-11-13 12:12:09 -0500

    comparing_bike_rentals_rain_or_shine.sql

---
+++ comparing_bike_rental_rain_or_shine.sql
@@ -0,0 +1,29 @@
+WITH rainy_days AS (
+ SELECT
+  date,
+  (MAX(prcp) > 5) AS rainy
+FROM (
+  SELECT
+    wx.date AS date,
+    IF (wx.element = 'PRCP', wx.value/10, NULL) AS prcp
+  FROM
+    `bigquery-public-data.ghcn_d.ghcnd_2016` AS wx
+  WHERE
+    wx.id = 'USW00094728'
+)
+GROUP BY
+ date
+),
+bicycle_rentals AS (
+  SELECT
+    COUNT(starttime) as num_trips,
+    EXTRACT(DATE from starttime) as trip_date
+ FROM `bigquery-public-data.new_york_citibike.citibike_trips`
+ GROUP BY trip_date
+)
+
+SELECT rd.rainy, ROUND(AVG(bk.num_trips),0) 
+FROM bicycle_rentals bk
+JOIN rainy_days rd ON
+bk.trip_date = rd.date
+GROUP BY rd.rainy
